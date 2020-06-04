5cafb21027077100ee4b2c820f79374818e2d169
cllefler <catherinelefler@yahoo.com>
2020-06-04 15:44:37 -0500

    commit last part

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -81,6 +81,12 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("appropriateFacials", appropriateFacials);
+
         return "menu";
     }
+
 }
